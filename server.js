32e2977e54b8c2c339e68cef783196e89092792e
mastan <mastanbee1994@gmail.com>
2026-01-07 12:30:06 +0530

    changes done 1

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use('/profilePics', express.static('profilePics'))
 
 app.use(express.static(path.join(__dirname,"./3tierdeploy/build")));
 
-app.get("*",(req,res)=>{res.sendfile(path.join(__dirname,"./3tierdeploy/build/index.html"))});
+app.get("/*",(req,res)=>{res.sendFile(path.join(__dirname,"./3tierdeploy/build/index.html"));
+});
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb){
