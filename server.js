108ec58ed9edf0414cc8ce38e3cb4b41543521fc
mastan <mastanbee1994@gmail.com>
2026-01-07 12:51:31 +0530

    changes done 1

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/profilePics', express.static('profilePics'))
 // });
 app.use(express.static(path.join(__dirname, "3tierdeploy", "build")));
 
-app.get("*", (req, res) => {
+app.get((req, res) => {
   res.sendFile(
     path.join(__dirname, "3tierdeploy", "build", "index.html")
   );
